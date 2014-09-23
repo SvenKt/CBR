8f1bb28f46fa11dccb0832f7a6737fa7beda6f59
Pascal Helbig <pascalhelbig@gmail.com>
2014-09-23 18:58:02 +0200

    eine fehlerhaften Test hinzugefügt

--- Tests/HammingTest.php
+++ Tests/HammingTest.php
@@ -3,4 +3,9 @@ class HammingTest extends PHPUnit_Framework_TestCase {
 	public function testHamming() {
 		$this->assertEquals(-1, -1);
 	}
+	
+	public function failureTest() {
+		// Sollte einem Fehler erzeugen
+		$this->assertEquals(0,1);
+	}
 }
