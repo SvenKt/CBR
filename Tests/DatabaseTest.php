d56244b6b93b63131d9d813bb51244208ac279da
Pascal Helbig <pascalhelbig@gmail.com>
2014-09-25 17:30:08 +0200

    DatabaseTest nun mit assertTrue

--- Tests/DatabaseTest.php
+++ Tests/DatabaseTest.php
@@ -3,12 +3,13 @@ include 'include/DB_Functions.php';
 include 'hamming.php';
 class DatabaseTest extends PHPUnit_Framework_TestCase {
 	public function testInsert() {
+		$db = new DB_Functions();
+		$this->assertTrue($db->createTable(), 'Tabelle konnte nicht erstellt werden');
 		$input = null;
 		foreach(Hamming::$attribute as $attribut) {
 			$input[$attribut['spalte']] = 0;
 		}	
-		$db = new DB_Functions();
-		$db->addSpeise($input, "Test");
+		$this->asserTrue($db->addSpeise($input, "Test"), 'Speisen konnten nicht hinzugefügt werden.');
 	}
 	
 }
