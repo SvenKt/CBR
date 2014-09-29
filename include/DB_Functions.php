468f7615a9c4b9280988a060086af5e6f35b5f9b
unknown <IT-112@IT-112.hwr.local>
2014-09-29 14:27:20 +0200

    query befehle verbessert

--- include/DB_Functions.php
+++ include/DB_Functions.php
@@ -3,6 +3,7 @@
 class DB_Functions {
  
     private $db;
+	private $con;
  
     //put your code here
     // constructor
@@ -10,13 +11,13 @@ class DB_Functions {
         require_once 'DB_Connect.php';
         // connecting to database
         $this->db = new DB_Connect();
-        $this->db->connect();
+        $this->con = $this->db->connect();
     }
      
 	public function getSpeisen() {
 		$sql = "SELECT * FROM ".DB_TABLE;
 		//$result = mysql_query($sql) or die ("Fehler");
-		$result = mysqli_query ($sql);// or die ("Fehler");
+		$result = $this->con->query($sql);// or die ("Fehler");
 		$speisen = null;
 		while($row = mysqli_fetch_assoc($result)) {
 			$speisen[] = $row;
@@ -30,12 +31,12 @@ class DB_Functions {
 			$sql.=$input[$attribut['spalte']]." , ";
 		}
 		$sql.="'".$neuesRezept."',NULL,1);";
-		return mysqli_query ($sql);
+		return $this->con->query($sql);
 	}
 	
 	public function createTable() {
 		$sql = "CREATE TABLE IF NOT EXISTS `rezept` (`id` int(11) NOT NULL AUTO_INCREMENT, `warm` int(11) DEFAULT NULL, `zeit` int(11) DEFAULT NULL, `personen` int(11) DEFAULT NULL, `gesund` int(11) DEFAULT NULL, `hunger` int(11) DEFAULT NULL, `vegetarisch` int(11) DEFAULT NULL, `kochen` int(11) DEFAULT NULL, `ergebnis` varchar(40) DEFAULT NULL, `flickr` varchar(50) DEFAULT NULL, beliebt int(11) DEFAULT 0, PRIMARY KEY (`id`))";
-		return mysqli_query ($sql);
+		return $this->con->query ($sql);
 	}
 }	
 ?>
