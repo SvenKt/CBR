baba9921b00404a654679fe591f029addd933c05
unknown <IT-113@IT-113.hwr.local>
2014-10-09 15:46:27 +0200

    Flickr Test V2

--- Tests/FlickrTest.php
+++ Tests/FlickrTest.php
@@ -2,20 +2,15 @@
 include_once "../flickr.php";
 
 class FlickrTest extends PHPUnit_Framework_TestCase {
-	$flickr = null;	
-	/**
-	* @before
-	**/
-	public function testInitialFlickr() {
-		$this->flickr = new Flickr(6122735488);
-	}
-	
+
 	public function testGetImage(){
-		$this->assertEquals('https://farm7.staticflickr.com/6202/6122735488_cb711a2d3f_o.jpg',$this->flickr->getImage(), 'Falsch/Keine Url erhalten');
+		$flickr = new Flickr(6122735488);
+		$this->assertEquals('https://farm7.staticflickr.com/6202/6122735488_cb711a2d3f_o.jpg',$flickr->getImage(), 'Falsch/Keine Url erhalten');
 	}
 	
 	public function testGetAuthor(){
-		$author = $this->flickr->getAuthor();
+		$flickr = new Flickr(6122735488);
+		$author = $flickr->getAuthor();
 		$this->assertEquals('kubina',$author['username'], 'Falschen/Keinen Author erhalten');
 		$this->assertEquals('https://www.flickr.com/photos/kubina/6122735488',$author['linkPicture'], 'Falsches/Kein linkPicture erhalten');
 	}
