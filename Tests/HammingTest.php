b2b018a554f5df6dd2abd88d428e8bc202e26dc4
unknown <IT-112@IT-112.hwr.local>
2014-10-09 14:40:30 +0200

    Hamming angepasst

--- Tests/HammingTest.php
+++ Tests/HammingTest.php
@@ -1,7 +1,18 @@
 <?php
+include 'include/DB_Functions.php';
+include 'hamming.php';
 class HammingTest extends PHPUnit_Framework_TestCase {
 	public function testHamming() {
-		$this->assertEquals(-1, -1);
+		$input = null;
+		foreach(Hamming::$attribute as $attribut) {
+			$input[$attribut['spalte']] = 0;
+		}
+		
+		$hamming = new Hamming();
+		$ergebnisse = $hamming->run($input);
+	
+		$this->assertEquals('null', $ergebnisse[0]['speise']);
+		$this->assertEquals('eins', $ergebnisse[1]['speise']);
 	}
 	
 }
