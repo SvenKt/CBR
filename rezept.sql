c3f2d68f9443add1644696a3c4ce481fe4e08a73
Pascal Helbig <pascalhelbig@gmail.com>
2014-10-29 10:50:32 +0100

    Datenbasis Update

--- rezept.sql
+++ rezept.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Erstellungszeit: 25. Sep 2014 um 12:45
--- Server Version: 5.5.16
--- PHP-Version: 5.3.8
+-- Host: 127.0.0.1
+-- Erstellungszeit: 29. Okt 2014 um 10:45
+-- Server Version: 5.6.20
+-- PHP-Version: 5.5.15
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `rezept` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `warm` int(11) DEFAULT NULL,
   `zeit` int(11) DEFAULT NULL,
   `personen` int(11) DEFAULT NULL,
@@ -37,65 +37,84 @@ CREATE TABLE IF NOT EXISTS `rezept` (
   `kochen` int(11) DEFAULT NULL,
   `ergebnis` varchar(40) DEFAULT NULL,
   `flickr` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `beliebt` int(11) DEFAULT '0'
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=51 ;
 
 --
 -- Daten für Tabelle `rezept`
 --
 
-INSERT INTO `rezept` (`id`, `warm`, `zeit`, `personen`, `gesund`, `hunger`, `vegetarisch`, `kochen`, `ergebnis`, `flickr`) VALUES
-(1, 1, 0, 0, 1, 0, 1, 0, 'Nudeln', '4630161702'),
-(2, 0, 0, 0, 1, 0, 1, 0, 'Belegtes Brot', '3231398503'),
-(3, 1, 1, 1, 0, 1, 0, 1, 'Hackbraten mit Gratin', '6223423877'),
-(4, 1, 1, 1, 0, 1, 0, 0, 'Schnitzel mit Pommes', '1277851411'),
-(5, 1, 1, 1, 1, 0, 1, 1, 'selbstgemachte Pizza', '345593444'),
-(6, 1, 1, 1, 1, 1, 1, 1, 'vegetarische Lasagne', '32878005'),
-(7, 1, 0, 1, 0, 1, 0, 0, 'Pizza', '8175276708'),
-(8, 1, 0, 0, 0, 1, 0, 1, 'Döner', '1057919169'),
-(9, 0, 0, 0, 1, 0, 1, 0, 'Käsebaguette', '10065748434'),
-(10, 1, 0, 0, 1, 0, 1, 0, 'Ofenkartoffel', '294567423'),
-(11, 1, 1, 1, 1, 1, 1, 0, 'Bratkartoffeln', '4915563719'),
-(12, 1, 1, 1, 1, 1, 0, 0, 'Bratkartoffeln mit Speck', '10009068436'),
-(13, 1, 0, 1, 0, 0, 1, 0, 'Nudelsuppe', '3225647903'),
-(14, 0, 1, 0, 1, 0, 1, 0, 'gemischter Salat', '8008364024'),
-(15, 1, 0, 1, 0, 1, 0, 0, 'Currywurst', '9070115198'),
-(16, 1, 0, 1, 0, 1, 0, 0, 'gekochte Mettwurst auf Brot', '7944526634'),
-(17, 1, 1, 1, 0, 1, 0, 1, 'Grünkohl mit Bregenwurst', '3338000079'),
-(18, 1, 0, 1, 0, 1, 0, 1, 'Spaghetti Bolognese', '4584807544'),
-(19, 0, 1, 0, 0, 0, 1, 1, 'Kandierter Apfel', '9375717941'),
-(20, 1, 1, 0, 1, 0, 1, 1, 'pikante Krabbensuppe', '2588103836'),
-(21, 1, 1, 1, 1, 1, 1, 1, 'frische gegrillte Forelle', '5474175409'),
-(22, 1, 0, 1, 0, 0, 1, 1, 'Fischstäbchen', '11243029303'),
-(23, 0, 1, 0, 1, 0, 1, 1, 'Garnelen mit Knoblauchdip', '14092884935'),
-(24, 1, 1, 1, 1, 0, 1, 1, 'Risotto', '8015147727'),
-(25, 1, 0, 0, 0, 0, 0, 0, 'Bockwurst mit Brötchen', '2690247730'),
-(26, 1, 1, 1, 1, 1, 1, 0, 'Kartoffelsuppe', '4040361533'),
-(27, 1, 1, 1, 0, 1, 1, 1, 'Kartoffelknödel', '8373809494'),
-(28, 1, 1, 1, 0, 1, 1, 1, 'Germknödel', '15143159606'),
-(29, 1, 0, 0, 1, 0, 1, 1, 'gegrillter Halloumikäse', '3986128995'),
-(30, 1, 1, 0, 0, 1, 0, 0, 'saftiges Schweinenackensteak', '4957864756'),
-(31, 1, 1, 0, 0, 0, 1, 1, 'gefüllte Teigtaschen mit Feta', '387152905'),
-(32, 1, 0, 0, 0, 0, 1, 0, 'Milchreis mit Zimt und Zucker', '6231417676'),
-(33, 1, 1, 1, 0, 1, 1, 1, 'Spätzle', '3321527700'),
-(34, 1, 1, 1, 0, 1, 0, 1, 'Spare Ribs mit Honigmarinade', '5850255554'),
-(35, 0, 1, 1, 0, 0, 1, 0, 'Schokokekse', '5471782652'),
-(36, 1, 1, 1, 0, 1, 1, 0, 'American Pancakes', '5969393821'),
-(37, 1, 1, 1, 0, 1, 0, 1, 'Erbseneintopf', '7353701816'),
-(38, 1, 1, 0, 0, 1, 0, 1, 'Kross gebackene Ente', '14164407403'),
-(39, 1, 1, 1, 0, 1, 0, 1, 'deftig belegte Langos', '4256771513'),
-(40, 1, 1, 1, 1, 0, 1, 1, 'Kürbiscremesuppe', '2305936243'),
-(41, 1, 1, 1, 0, 1, 1, 1, 'indisches Reiscurry', '4701521386'),
-(42, 0, 1, 1, 1, 1, 1, 0, 'Meeresfrüchteplatte', '7976347081'),
-(43, 1, 1, 1, 1, 1, 1, 1, 'spanische Paella', '12533718105'),
-(44, 0, 1, 0, 0, 0, 0, 1, 'Datteln im Speckmantel', '12255513543'),
-(45, 0, 0, 1, 1, 0, 1, 1, 'Gazpacho', '2824332804'),
-(46, 1, 0, 0, 1, 0, 1, 1, 'Minestrone', '4309005328'),
-(47, 1, 0, 0, 1, 0, 0, 0, 'leichte Hühnerbrühe', '8347114335'),
-(48, 0, 0, 1, 1, 0, 1, 1, 'Couscous', '4429274459'),
-(49, 1, 1, 0, 1, 1, 0, 0, 'gegrilltes Hähnchenfilet', '7520970416'),
-(50, 1, 0, 0, 0, 1, 0, 0, 'Türkische Pizza mit Salat', '4029896104');
+INSERT INTO `rezept` (`id`, `warm`, `zeit`, `personen`, `gesund`, `hunger`, `vegetarisch`, `kochen`, `ergebnis`, `flickr`, `beliebt`) VALUES
+(1, 1, 0, 0, 1, 0, 1, 0, 'Nudeln', '4630161702', 0),
+(2, 0, 0, 0, 1, 0, 1, 0, 'Belegtes Brot', '3231398503', 0),
+(3, 1, 1, 1, 0, 1, 0, 1, 'Hackbraten mit Gratin', '6223423877', 0),
+(4, 1, 1, 1, 0, 1, 0, 0, 'Schnitzel mit Pommes', '1277851411', 0),
+(5, 1, 1, 1, 1, 0, 1, 1, 'selbstgemachte Pizza', '345593444', 0),
+(6, 1, 1, 1, 1, 1, 1, 1, 'vegetarische Lasagne', '32878005', 0),
+(7, 1, 0, 1, 0, 1, 0, 0, 'Pizza', '8175276708', 0),
+(8, 1, 0, 0, 0, 1, 0, 1, 'Döner', '1057919169', 0),
+(9, 0, 0, 0, 1, 0, 1, 0, 'Käsebaguette', '10065748434', 0),
+(10, 1, 0, 0, 1, 0, 1, 0, 'Ofenkartoffel', '294567423', 0),
+(11, 1, 1, 1, 1, 1, 1, 0, 'Bratkartoffeln', '4915563719', 0),
+(12, 1, 1, 1, 1, 1, 0, 0, 'Bratkartoffeln mit Speck', '10009068436', 0),
+(13, 1, 0, 1, 0, 0, 1, 0, 'Nudelsuppe', '3225647903', 0),
+(14, 0, 1, 0, 1, 0, 1, 0, 'gemischter Salat', '8008364024', 0),
+(15, 1, 0, 1, 0, 1, 0, 0, 'Currywurst', '9070115198', 0),
+(16, 1, 0, 1, 0, 1, 0, 0, 'gekochte Mettwurst auf Brot', '7944526634', 0),
+(17, 1, 1, 1, 0, 1, 0, 1, 'Grünkohl mit Bregenwurst', '3338000079', 0),
+(18, 1, 0, 1, 0, 1, 0, 1, 'Spaghetti Bolognese', '4584807544', 0),
+(19, 0, 1, 0, 0, 0, 1, 1, 'Kandierter Apfel', '9375717941', 0),
+(20, 1, 1, 0, 1, 0, 1, 1, 'pikante Krabbensuppe', '2588103836', 0),
+(21, 1, 1, 1, 1, 1, 1, 1, 'frische gegrillte Forelle', '5474175409', 0),
+(22, 1, 0, 1, 0, 0, 1, 1, 'Fischstäbchen', '11243029303', 0),
+(23, 0, 1, 0, 1, 0, 1, 1, 'Garnelen mit Knoblauchdip', '14092884935', 0),
+(24, 1, 1, 1, 1, 0, 1, 1, 'Risotto', '8015147727', 0),
+(25, 1, 0, 0, 0, 0, 0, 0, 'Bockwurst mit Brötchen', '2690247730', 0),
+(26, 1, 1, 1, 1, 1, 1, 0, 'Kartoffelsuppe', '4040361533', 0),
+(27, 1, 1, 1, 0, 1, 1, 1, 'Kartoffelknödel', '8373809494', 0),
+(28, 1, 1, 1, 0, 1, 1, 1, 'Germknödel', '15143159606', 0),
+(29, 1, 0, 0, 1, 0, 1, 1, 'gegrillter Halloumikäse', '3986128995', 0),
+(30, 1, 1, 0, 0, 1, 0, 0, 'saftiges Schweinenackensteak', '4957864756', 0),
+(31, 1, 1, 0, 0, 0, 1, 1, 'gefüllte Teigtaschen mit Feta', '387152905', 0),
+(32, 1, 0, 0, 0, 0, 1, 0, 'Milchreis mit Zimt und Zucker', '6231417676', 0),
+(33, 1, 1, 1, 0, 1, 1, 1, 'Spätzle', '3321527700', 0),
+(34, 1, 1, 1, 0, 1, 0, 1, 'Spare Ribs mit Honigmarinade', '5850255554', 0),
+(35, 0, 1, 1, 0, 0, 1, 0, 'Schokokekse', '5471782652', 0),
+(36, 1, 1, 1, 0, 1, 1, 0, 'American Pancakes', '5969393821', 0),
+(37, 1, 1, 1, 0, 1, 0, 1, 'Erbseneintopf', '7353701816', 0),
+(38, 1, 1, 0, 0, 1, 0, 1, 'Kross gebackene Ente', '14164407403', 0),
+(39, 1, 1, 1, 0, 1, 0, 1, 'deftig belegte Langos', '4256771513', 0),
+(40, 1, 1, 1, 1, 0, 1, 1, 'Kürbiscremesuppe', '2305936243', 0),
+(41, 1, 1, 1, 0, 1, 1, 1, 'indisches Reiscurry', '4701521386', 0),
+(42, 0, 1, 1, 1, 1, 1, 0, 'Meeresfrüchteplatte', '7976347081', 0),
+(43, 1, 1, 1, 1, 1, 1, 1, 'spanische Paella', '12533718105', 0),
+(44, 0, 1, 0, 0, 0, 0, 1, 'Datteln im Speckmantel', '12255513543', 0),
+(45, 0, 0, 1, 1, 0, 1, 1, 'Gazpacho', '2824332804', 0),
+(46, 1, 0, 0, 1, 0, 1, 1, 'Minestrone', '4309005328', 0),
+(47, 1, 0, 0, 1, 0, 0, 0, 'leichte Hühnerbrühe', '8347114335', 0),
+(48, 0, 0, 1, 1, 0, 1, 1, 'Couscous', '4429274459', 0),
+(49, 1, 1, 0, 1, 1, 0, 0, 'gegrilltes Hähnchenfilet', '7520970416', 0),
+(50, 1, 0, 0, 0, 1, 0, 0, 'Türkische Pizza mit Salat', '4029896104', 0);
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `rezept`
+--
+ALTER TABLE `rezept`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `rezept`
+--
+ALTER TABLE `rezept`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=51;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
